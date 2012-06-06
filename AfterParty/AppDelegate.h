97e8847180b6450ddbe19e28956c01541704e5ef
suraj mirajkar <suraj@weboniselab.com>
2012-06-06 16:29:44 +0530

    Initial Commit

---
+++ AfterParty/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AfterParty
+//
+//  Created by Bhuvan Khanna on 06/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
