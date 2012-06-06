6714a6d120cca5171fc3f63b2c1101cf2d1ae7b5
suraj mirajkar <suraj@weboniselab.com>
2012-06-06 19:22:08 +0530

    Images Added to Preferences Screen and added functionality.

--- AfterParty/PreferencesViewController.h
+++ AfterParty/PreferencesViewController.h
@@ -8,14 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PreferencesViewController : UIViewController
+@interface PreferencesViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 {
+    NSUserDefaults *userPreference;
     
 }
 @property (retain, nonatomic) IBOutlet UIButton *iAmMale;
 @property (retain, nonatomic) IBOutlet UIButton *iAmFemale;
 @property (retain, nonatomic) IBOutlet UIButton *lookingForMale;
 @property (retain, nonatomic) IBOutlet UIButton *lookingForFemale;
+
+
+- (IBAction)maleSelectAction:(id)sender;
+- (IBAction)femaleSelectAction:(id)sender;
+- (IBAction)lookingforMaleSelectAction:(id)sender;
+- (IBAction)lookingforFemaleSelectAction:(id)sender;
+
+
 - (IBAction)TakeAPicture:(id)sender;
 
 @end
--- AfterParty/PreferencesViewController.m
+++ AfterParty/PreferencesViewController.m
@@ -30,6 +30,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    userPreference = [NSUserDefaults standardUserDefaults];
+    [userPreference removeObjectForKey:@"IamMALE"];
+    [userPreference removeObjectForKey:@"IamFEMALE"];
+    [userPreference removeObjectForKey:@"SeekMALE"];
+    [userPreference removeObjectForKey:@"SeekFEMALE"];
     
 }
 
@@ -58,8 +63,149 @@
     [lookingForFemale release];
     [super dealloc];
 }
+
+- (IBAction)maleSelectAction:(id)sender 
+{
+    if ([iAmMale isSelected]) 
+    {
+        [iAmMale setImage:[UIImage imageNamed:@"MaleUnselected.png"] forState:UIControlStateNormal];
+        [iAmMale setSelected:NO];
+        
+        [userPreference setObject:@"no" forKey:@"IamMALE"];
+        
+    }
+    else
+    {
+        [iAmMale setImage:[UIImage imageNamed:@"MaleSelected.png"] forState:UIControlStateSelected];
+        [iAmMale setSelected:YES];
+        [iAmFemale setImage:[UIImage imageNamed:@"FemaleUnselected.png"] forState:UIControlStateNormal];
+        [iAmFemale setSelected:NO];
+        
+        [userPreference setObject:@"yes" forKey:@"IamMALE"];
+        [userPreference setObject:@"no" forKey:@"IamFEMALE"];
+       
+    }
+    [userPreference synchronize];
+
+}
+
+- (IBAction)femaleSelectAction:(id)sender 
+{
+    if ([iAmFemale isSelected]) 
+    {
+        [iAmFemale setImage:[UIImage imageNamed:@"FemaleUnselected.png"] forState:UIControlStateNormal];
+        [iAmFemale setSelected:NO];
+        
+        [userPreference setObject:@"no" forKey:@"IamFEMALE"];
+        
+    }
+    else
+    {
+        [iAmFemale setImage:[UIImage imageNamed:@"FemaleSelected.png"] forState:UIControlStateSelected];
+        [iAmFemale setSelected:YES];
+        [iAmMale setImage:[UIImage imageNamed:@"MaleUnselected.png"] forState:UIControlStateNormal];
+        [iAmMale setSelected:NO];
+        
+        [userPreference setObject:@"yes" forKey:@"IamFEMALE"];
+        [userPreference setObject:@"no" forKey:@"IamMALE"];
+    }
+    [userPreference synchronize];
+}
+
+- (IBAction)lookingforMaleSelectAction:(id)sender 
+{
+    if ([lookingForMale isSelected]) 
+    {
+        [lookingForMale setImage:[UIImage imageNamed:@"MaleUnselected.png"] forState:UIControlStateNormal];
+        [lookingForMale setSelected:NO];
+        
+        [userPreference setObject:@"no" forKey:@"SeekMALE"];
+        
+    }
+    else
+    {
+        [lookingForMale setImage:[UIImage imageNamed:@"MaleSelected.png"] forState:UIControlStateSelected];
+        [lookingForMale setSelected:YES];
+        [lookingForFemale setImage:[UIImage imageNamed:@"FemaleUnselected.png"] forState:UIControlStateNormal];
+        [lookingForFemale setSelected:NO];
+        
+        [userPreference setObject:@"yes" forKey:@"SeekMALE"];
+        [userPreference setObject:@"no" forKey:@"SeekFEMALE"];
+        
+    }
+    [userPreference synchronize];
+}
+
+- (IBAction)lookingforFemaleSelectAction:(id)sender 
+{
+    if ([lookingForFemale isSelected]) 
+    {
+        [lookingForFemale setImage:[UIImage imageNamed:@"FemaleUnselected.png"] forState:UIControlStateNormal];
+        [lookingForFemale setSelected:NO];
+        
+        [userPreference setObject:@"no" forKey:@"SeekFEMALE"];
+    }
+    else
+    {
+        [lookingForFemale setImage:[UIImage imageNamed:@"FemaleSelected.png"] forState:UIControlStateSelected];
+        [lookingForFemale setSelected:YES];
+        [lookingForMale setImage:[UIImage imageNamed:@"MaleUnselected.png"] forState:UIControlStateNormal];
+        [lookingForMale setSelected:NO];
+        
+        [userPreference setObject:@"yes" forKey:@"SeekFEMALE"];
+        [userPreference setObject:@"no" forKey:@"SeekMALE"];
+        
+    }
+    [userPreference synchronize];
+}
+
 - (IBAction)TakeAPicture:(id)sender 
 {
     
+    
+    NSLog(@"IamMALE %@",[userPreference objectForKey:@"IamMALE"]);
+    NSLog(@"IamFEMALE %@",[userPreference objectForKey:@"IamFEMALE"]);
+    
+    NSLog(@"SeekMALE %@",[userPreference objectForKey:@"SeekMALE"]);
+    NSLog(@"SeekFEMALE %@",[userPreference objectForKey:@"SeekFEMALE"]);
+    
+    if ( ([[userPreference objectForKey:@"IamMALE"] isEqualToString:@"yes"] || [[userPreference objectForKey:@"IamFEMALE"] isEqualToString:@"yes"]) && ([[userPreference objectForKey:@"SeekMALE"] isEqualToString:@"yes"] || [[userPreference objectForKey:@"SeekFEMALE"] isEqualToString:@"yes"]) ) 
+    {
+        NSLog(@"IamMALE %@",[userPreference objectForKey:@"IamMALE"]);
+        NSLog(@"IamFEMALE %@",[userPreference objectForKey:@"IamFEMALE"]);
+        
+        NSLog(@"SeekMALE %@",[userPreference objectForKey:@"SeekMALE"]);
+        NSLog(@"SeekFEMALE %@",[userPreference objectForKey:@"SeekFEMALE"]);
+    }
+    
+  /*  if (([iAmMale isSelected]  || [iAmFemale isSelected] ) && ([lookingForMale isSelected] || [lookingForFemale isSelected])) 
+    {
+        NSLog(@"Open Camera");
+        UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+        
+        // Set source to the camera
+        imagePicker.sourceType =  UIImagePickerControllerSourceTypeCamera;
+        
+        // Delegate is self
+        imagePicker.delegate = self;
+        
+        // Allow editing of image ?
+       // imagePicker.allowsImageEditing = NO;
+        
+        // Show image picker
+        [self presentModalViewController:imagePicker animated:YES];
+    }*/
 }
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    // Access the uncropped image from info dictionary
+    UIImage *image = [info objectForKey:@"Image"];
+    
+    // Save image
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    
+    [picker release];
+}
+
 @end
